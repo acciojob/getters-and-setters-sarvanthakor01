3aecab7af626e13404d6a97ece6d95311627ee76
Sarvan Thakor <97446457+sarvanthakor01@users.noreply.github.com>
2025-02-16 17:43:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 //complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  get age() {
+    return this._age;
+  }
+
+  set age(newAge) {
+    if (newAge > 0) {
+      this._age = newAge;
+    } else {
+      throw new Error("Age must be a positive number.");
+    }
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(`${this._name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this._name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
